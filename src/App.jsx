7979487a6cf5c9bc2d1b222418a90399305a11c0
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-02-06 02:51:58 +0530

    made cardComponets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Food from "./components/Food";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Card from "./components/Card";
 
 function App() {
 	return (
 		<>
-			<Header />
+    <Card/>
+			{/* <Header />
       <Food/>
-      <Footer/>
+      <Footer/> */}
 		</>
 	);
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import nagi from '../assets/ss.jpg'
+
+const Card = () => {
+  return (
+    <div className="card">
+        <img className='card-image' src={nagi} alt="picture"></img>
+        <h2 className='card-title'>fuckup</h2>
+        <p className='card-text'>learning to do some thing babe</p>
+    </div>
+  )
+}
+
+export default Card
